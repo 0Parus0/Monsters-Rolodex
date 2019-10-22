f27e68bc9663452f67ef5edbd52650f5c71cd87c
Parus <0Parus0@gmail.com>
2019-10-22 15:42:30 +0500

    Updates

---
+++ precache-manifest.f8af59d04cc32c2f3e6211d551f4cbfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f627d94b8464880cc74c1e342bd8f039",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c60e4d0cfcd36e5ef38d",
+    "url": "/monsters-rolodex/static/css/main.889543e0.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "c60e4d0cfcd36e5ef38d",
+    "url": "/monsters-rolodex/static/js/main.e20adead.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
